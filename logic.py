9e4ae911af75d671b4ce07c7e9807ad997b3444c
Baraciak <baraciak@gmail.com>
2019-05-29 10:55:53 +0200

    poprawki bo KJ bo brzydko pisze

--- logic.py
+++ logic.py
@@ -4,7 +4,6 @@ from data import db_connection
 import login
 
 
-
 def get_board_list():
     board_dict = queries.get_boards()
     return board_dict
@@ -13,20 +12,19 @@ def get_board_list():
 def get_columns_with_tasks_by_board_id(board_id):
     columns = queries.get_columns_to_board(board_id)
     tasks = queries.get_tasks_to_board_and_column(board_id)
-    # cards = {
-    #     columns:
-    # }
     return columns, tasks
 
 
 def test_db_conn():
     print(db_connection.test_connection_db())
 
+
 def save_new_user_data(user_data):
     hashed_pass = login.hash_password(user_data['password'])
     user_data['password'] = hashed_pass
     added = login.save_new_user_data(user_data)
     return added
+
 
 def add_new_board():
     board_data = {
@@ -35,11 +33,12 @@ def add_new_board():
     }
     return queries.add_board(board_data)
 
+
 def add_fixed_columns(board_id):
-    columns = [{'title':'NEW','board_id':board_id},
-               {'title':'TO DO','board_id':board_id},
-               {'title':'IN PROGRESS','board_id':board_id},
-               {'title':'DONE','board_id':board_id}]
+    columns = [{'title': 'NEW', 'board_id': board_id},
+               {'title': 'TO DO', 'board_id': board_id},
+               {'title': 'IN PROGRESS', 'board_id': board_id},
+               {'title': 'DONE', 'board_id': board_id}]
     for column in columns:
         queries.add_column(column)
 
